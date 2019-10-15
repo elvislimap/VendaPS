14de2b1dd9e328e64666f976b334d0c4143786c4
elvislimap <elvislimap@gmail.com>
2019-10-15 14:52:16 -0300

    .

--- src/VendaPS.Domain/Entities/Client.cs
+++ src/VendaPS.Domain/Entities/Client.cs
@@ -5,11 +5,6 @@ namespace VendaPS.Domain.Entities
 {
     public class Client
     {
-        public Client()
-        {
-            
-        }
-
         public int ClientId { get; set; }
         public string Cpf { get; set; }
         public string Name { get; set; }
